b83b638f86fd5296422a634879afd89720d2fab0
Mark Bloomfield <mark@yellow-llama.com>
2025-11-21 16:27:15 +0200

    allow localhost images

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,20 @@ const nextConfig: NextConfig = {
       {
         protocol: "http",
         hostname: "127.0.0.1",
+        port: "",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+      {
+        protocol: "http",
+        hostname: "[::1]",
       },
     ],
   },
